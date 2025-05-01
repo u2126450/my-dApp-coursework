11bce5749c81484f2d69871482e6e62a12f63b42
Abbos <abboskhon6402@gmail.com>
2025-05-01 12:33:33 +0100

    Final project: frontend + Sepolia integration + config cleanup

--- defi-staking-app-final-code-project/truffle-config.js
+++ defi-staking-app-final-code-project/truffle-config.js
@@ -1,13 +1,27 @@
 require('babel-register');
 require('babel-polyfill');
+require('dotenv').config();
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*"
     },
+    sepolia: {
+      provider: () => new HDWalletProvider(
+        process.env.MNEMONIC,
+        `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`
+      ),
+      network_id: 11155111,
+      gas: 5500000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    }
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/truffle_abis/',
@@ -20,6 +34,6 @@ module.exports = {
       },
     }
   }
-}
+};
 
 
